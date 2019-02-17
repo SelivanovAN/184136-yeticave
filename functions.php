7068b2f0e0ab28811a6b7039ce66c1cfe1c6f884
SelivanovAN <superexpulsion@mail.ru>
2019-02-17 12:30:34 +0300

    Сделано задание по отображению страницы лота

--- functions.php
+++ functions.php
@@ -1,4 +1,35 @@
 <?php
+
+function space_price($price) {
+    $around_price = ceil($price);
+    if ($around_price > 1000) {
+        $make_space = number_format ($around_price, 0, 0, " ");
+        return $make_space ." ₽";
+    }
+    return $around_price ." ₽";
+};
+
+function check_hakers($typing) {
+    $haker = strip_tags($typing);
+    return $haker;
+};
+
+function show_date() {
+    $date_now = date_create("now");
+    $date_next = date_create("tomorrow");
+    $date_diff = date_diff($date_next, $date_now);
+    $date_count = date_interval_format($date_diff, "%h:%i");
+    return $date_count;
+};
+
+function show_date_close($date) {
+    $date_now = date_create("now");
+    $date_next = date_create($date);
+    $date_diff = date_diff($date_next, $date_now);
+    $date_count = date_interval_format($date_diff, "%h:%i");
+    return $date_count;
+};
+
 function include_template($name, $data) {
     $name = 'templates/' . $name;
     $result = '';
