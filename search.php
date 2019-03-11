0353d6e3e5fd7e8fa759ca9a62d35913b8b62f99
SelivanovAN <superexpulsion@mail.ru>
2019-03-11 22:07:12 +0300

    Мелкая перестановка

--- search.php
+++ search.php
@@ -6,30 +6,8 @@ include_once 'functions.php';
 $link = connect_to_db();
 $title = return_name_title();
 
-/*
-$lots_select = [];
-
-if($link) {
-    $lots_sql = 'SELECT l.id, l.name, l.start_price, l.picture_link, MAX(b.price_buy), MAX(c.name), l.date_create, l.date_close, l.description
-    FROM lots l JOIN category c ON l.id_category = c.id LEFT JOIN bets b ON l.id = b.id_lot WHERE l.date_close > NOW() GROUP BY l.id ORDER BY l.date_create DESC';
-
-    $result_select = mysqli_query($link, $lots_sql);
-
-    if ($result_select) {
-        $lots_select = mysqli_fetch_all($result_select, MYSQLI_ASSOC);
-
-    } else {
-        print ('Произошла ошибка при выполнении запроса! Обратитесь к администратору, либо попробуйте снова.');
-        die();
-    }
-} else {
-    print ('Произошла ошибка подключения, недоступна база данных! Обратитесь к администратору, либо попробуйте снова.');
-    die();
-}
-*/
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$form_search = $_GET['search'] ?? [];
+	$form_search = $_GET['search'] ?? '';
 /*
 	$required = ['cost'];
     $dict = ['cost' => 'Ставка'];
@@ -41,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         }
     }*/
 
-    $lots = [];
+    $lots_select = [];
 
 	//$search = $_GET['q'] ?? '';
 
@@ -57,10 +35,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 		mysqli_stmt_execute($stmt);
 		$result = mysqli_stmt_get_result($stmt);
 
-		$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+		$lots_select = mysqli_fetch_all($result, MYSQLI_ASSOC);
 	}
 
-	$content_main = include_template('search.php', ['lots' => $lots, 'categories_select' => show_categories_select(), 'lots_select' => $lots_select]);
+	$content_main = include_template('search.php', ['categories_select' => show_categories_select(), 'lots_select' => $lots_select]);
+
+
+    /*
+    $lots_select = [];
+
+    if($link) {
+        $lots_sql = 'SELECT l.id, l.name, l.start_price, l.picture_link, MAX(b.price_buy), MAX(c.name), l.date_create, l.date_close, l.description
+        FROM lots l JOIN category c ON l.id_category = c.id LEFT JOIN bets b ON l.id = b.id_lot WHERE l.date_close > NOW() GROUP BY l.id ORDER BY l.date_create DESC';
+
+        $result_select = mysqli_query($link, $lots_sql);
+
+        if ($result_select) {
+            $lots_select = mysqli_fetch_all($result_select, MYSQLI_ASSOC);
+
+        } else {
+            print ('Произошла ошибка при выполнении запроса! Обратитесь к администратору, либо попробуйте снова.');
+            die();
+        }
+    } else {
+        print ('Произошла ошибка подключения, недоступна база данных! Обратитесь к администратору, либо попробуйте снова.');
+        die();
+    }
+    */
 
 /*
     if (!empty($lot_select['MAX(b.price_buy)'])) {
