c1388a2b12ab5cf46b0270ca9a0e52a6797c9155
Selivanov <superexpulsion@mail.ru>
2019-03-13 11:41:30 +0300

    Надо доделать пагинацию

--- search.php
+++ search.php
@@ -6,7 +6,6 @@ include_once 'functions.php';
 $link = connect_to_db();
 $title = return_name_title();
 
-
 	$form_search = trim($_GET['search'] ?? '');
 
     $lots_select = [];
@@ -14,47 +13,41 @@ $title = return_name_title();
 	if ($form_search) {
         $lots_sql = "SELECT l.id, l.name, l.start_price, l.picture_link, MAX(b.price_buy), MAX(c.name), l.date_create, l.date_close, l.description
         FROM lots l JOIN category c ON l.id_category = c.id LEFT JOIN bets b ON l.id = b.id_lot WHERE l.date_close > NOW() AND MATCH(l.name, l.description) AGAINST(?)
-        GROUP BY l.id ORDER BY l.date_create DESC LIMIT . $page_items . ' OFFSET ' . $offset";
-
-
+        GROUP BY l.id ORDER BY l.date_create DESC";
 
 		$stmt = db_get_prepare_stmt($link, $lots_sql, [$form_search]); // подготовка запроса
 		mysqli_stmt_execute($stmt); // выполнение запроса
 		$lots_select = mysqli_stmt_get_result($stmt); // получение результата
 
-        // VAR_dump($lots_select);
-        // die();
 	}
+    
+    $cur_page = $_GET['page'] ?? 1;
+    $page_items = 9;
 
-	$content_main = include_template('search.php', ['categories_select' => show_categories_select(), 'lots_select' => $lots_select, 'form_search' => $form_search]);
+    // $result = mysqli_query($link, "SELECT COUNT(*) as cnt FROM lots");
+    $items_count = mysqli_fetch_assoc($lots_select);
+    // $items_count = mysqli_fetch_assoc($result)['cnt'];
 
+    $pages_count = ceil($items_count / $page_items);
+    $offset = ($cur_page - 1) * $page_items;
 
+    $pages = range(1, $pages_count);
 
-/*
-    if (!empty($lot_select['MAX(b.price_buy)'])) {
-        $current_bet = $lot_select['MAX(b.price_buy)'] + $lot_select['step_bet'];
+    // запрос на показ результата поиска с пагинацией
+    $lots_pagination_sql = "SELECT l.id, l.name, l.start_price, l.picture_link, MAX(b.price_buy), MAX(c.name), l.date_create, l.date_close, l.description
+        FROM lots l JOIN category c ON l.id_category = c.id LEFT JOIN bets b ON l.id = b.id_lot WHERE l.date_close > NOW() AND MATCH(l.name, l.description) AGAINST(?)
+        GROUP BY l.id ORDER BY l.date_create DESC LIMIT . $page_items . ' OFFSET ' . $offset";
+
+    if ($lots = mysqli_query($link, $lots_pagination_sql)) {
+        $tpl_data = ['lots' => $lots, 'pages' => $pages, 'pages_count' => $pages_count, 'cur_page' => $cur_page];
+
+        $content = include_template('main.php', $tpl_data);
     }
     else {
-        $current_bet = $lot_select['start_price'] + $lot_select['step_bet'];
+        show_error($content, mysqli_error($link));
     }
 
-    if (!count($errors) && $form_add_bet['cost'] < $current_bet) {
-         $errors['cost'] = 'ставка не должа быть меньше текущей цены';
-    }*/
-
-    /*if (count($errors) == 0) {
-        $bet_add = 'INSERT INTO bets (price_buy, id_user, id_lot) VALUES (?, ?, ?)';
-        $stmt = db_get_prepare_stmt($link, $bet_add, [$form_add_bet['cost'], $_SESSION['user']['id'], $lot_id]);
-        $res = mysqli_stmt_execute($stmt);
-
-        if ($res && empty($errors)) {
-            header("Location: /lot.php?value_id=$lot_id");
-            exit();
-        }
-    }*/
-
-
-
+$content_main = include_template('search.php', ['categories_select' => show_categories_select(), 'lots_select' => $lots_select, 'form_search' => $form_search]);
 
 $layout = include_template ('layout.php', ['title' => $title['search'], 'categories_select' => show_categories_select(), 'content_main' => $content_main]);
 
